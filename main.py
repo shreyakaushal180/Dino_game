01f4dbaa929a7e21ec34a1169a566d097ebcd5bb
shreyakaushal180 <shreyakaushal118@gmail.com>
2024-10-23 08:33:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-from tkinter import*
-from tkinter import ttk
-from PIL import Image,ImageTk
 
-
-class Face_recognition_system:
-    def __init__(self,root):
-        self.root=root
-        self.root.geometry("1530x790+0+0")
-        self.root.title("face recognition system")
         
     
