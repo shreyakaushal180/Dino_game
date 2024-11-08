d23409df11cfd4154b0f9a81c592ee7ffdf75b71
burningblade44 <priyanshpal33@gmail.com>
2024-11-09 02:29:12 +0530

    Update game.py

--- Dino/game.py
+++ Dino/game.py
@@ -194,158 +194,122 @@ def main():
         clock.tick(30)
         pygame.display.update()
 <<<<<<< HEAD
-#preload obstacles
-=======
-               
 
-#                extends Node
+	class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
 
-# #preload obstacles
->>>>>>> 628090bbc5a7bbcd93c1d958d771808ed2c567d6
-# var stump_scene = preload("res://scenes/stump.tscn")
-# var rock_scene = preload("res://scenes/rock.tscn")
-# var barrel_scene = preload("res://scenes/barrel.tscn")
-# var bird_scene = preload("res://scenes/bird.tscn")
-# var obstacle_types := [stump_scene, rock_scene, barrel_scene]
-# var obstacles : Array
-# var bird_heights := [200, 390]
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
 
-# #game variables
-# const DINO_START_POS := Vector2i(150, 485)
-# const CAM_START_POS := Vector2i(576, 324)
-# var difficulty
-# const MAX_DIFFICULTY : int = 2
-# var score : int
-# const SCORE_MODIFIER : int = 10
-# var high_score : int
-# var speed : float
-# const START_SPEED : float = 10.0
-# const MAX_SPEED : int = 25
-# const SPEED_MODIFIER : int = 5000
-# var screen_size : Vector2i
-# var ground_height : int
-# var game_running : bool
-# var last_obs
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
 
-# # Called when the node enters the scene tree for the first time.
-# func _ready():
-# 	screen_size = get_window().size
-# 	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
-# 	$GameOver.get_node("Button").pressed.connect(new_game)
-# 	new_game()
 
-# func new_game():
-# 	#reset variables
-# 	score = 0
-# 	show_score()
-# 	game_running = false
-# 	get_tree().paused = false
-# 	difficulty = 0
-	
-# 	#delete all obstacles
-# 	for obs in obstacles:
-# 		obs.queue_free()
-# 	obstacles.clear()
-	
-# 	#reset the nodes
-# 	$Dino.position = DINO_START_POS
-# 	$Dino.velocity = Vector2i(0, 0)
-# 	$Camera2D.position = CAM_START_POS
-# 	$Ground.position = Vector2i(0, 0)
-	
-# 	#reset hud and game over screen
-# 	$HUD.get_node("StartLabel").show()
-# 	$GameOver.hide()
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
 
-# # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-# 	if game_running:
-# 		#speed up and adjust difficulty
-# 		speed = START_SPEED + score / SPEED_MODIFIER
-# 		if speed > MAX_SPEED:
-# 			speed = MAX_SPEED
-# 		adjust_difficulty()
-		
-# 		#generate obstacles
-# 		generate_obs()
-		
-# 		#move dino and camera
-# 		$Dino.position.x += speed
-# 		$Camera2D.position.x += speed
-		
-# 		#update score
-# 		score += speed
-# 		show_score()
-		
-# 		#update ground position
-# 		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-# 			$Ground.position.x += screen_size.x
-			
-# 		#remove obstacles that have gone off screen
-# 		for obs in obstacles:
-# 			if obs.position.x < ($Camera2D.position.x - screen_size.x):
-# 				remove_obs(obs)
-# 	else:
-# 		if Input.is_action_pressed("ui_accept"):
-# 			game_running = true
-# 			$HUD.get_node("StartLabel").hide()
 
-# func generate_obs():
-# 	#generate ground obstacles
-# 	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300, 500):
-# 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
-# 		var obs
-# 		var max_obs = difficulty + 1
-# 		for i in range(randi() % max_obs + 1):
-# 			obs = obs_type.instantiate()
-# 			var obs_height = obs.get_node("Sprite2D").texture.get_height()
-# 			var obs_scale = obs.get_node("Sprite2D").scale
-# 			var obs_x : int = screen_size.x + score + 100 + (i * 100)
-# 			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
-# 			last_obs = obs
-# 			add_obs(obs, obs_x, obs_y)
-# 		#additionally random chance to spawn a bird
-# 		if difficulty == MAX_DIFFICULTY:
-# 			if (randi() % 2) == 0:
-# 				#generate bird obstacles
-# 				obs = bird_scene.instantiate()
-# 				var obs_x : int = screen_size.x + score + 100
-# 				var obs_y : int = bird_heights[randi() % bird_heights.size()]
-# 				add_obs(obs, obs_x, obs_y)
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
 
-# func add_obs(obs, x, y):
-# 	obs.position = Vector2i(x, y)
-# 	obs.body_entered.connect(hit_obs)
-# 	add_child(obs)
-# 	obstacles.append(obs)
 
-# func remove_obs(obs):
-# 	obs.queue_free()
-# 	obstacles.erase(obs)
-	
-# func hit_obs(body):
-# 	if body.name == "Dino":
-# 		game_over()
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 250
+        self.index = 0
 
-# func show_score():
-# 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index//5], self.rect)
+        self.index += 1
 
-# func check_high_score():
-# 	if score > high_score:
-# 		high_score = score
-# 		$HUD.get_node("HighScoreLabel").text = "HIGH SCORE: " + str(high_score / SCORE_MODIFIER)
 
-# func adjust_difficulty():
-# 	difficulty = score / SPEED_MODIFIER
-# 	if difficulty > MAX_DIFFICULTY:
-# 		difficulty = MAX_DIFFICULTY
+def main():
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
+    run = True
+    clock = pygame.time.Clock()
+    player = Dinosaur()
+    cloud = Cloud()
+    game_speed = 20
+    x_pos_bg = 0
+    y_pos_bg = 380
+    points = 0
+    font = pygame.font.Font('freesansbold.ttf', 20)
+    obstacles = []
+    death_count = 0
 
-# func game_over():
-# 	check_high_score()
-# 	get_tree().paused = true
-# 	game_running = false
-# 	$GameOver.show()
+    def score():
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed += 1
 
+        text = font.render("Points: " + str(points), True, (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (1000, 40)
+        SCREEN.blit(text, textRect)
+
+    def background():
+        global x_pos_bg, y_pos_bg
+        image_width = BG.get_width()
+        SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
+        SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+        if x_pos_bg <= -image_width:
+            SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+            x_pos_bg = 0
+        x_pos_bg -= game_speed
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        SCREEN.fill((255, 255, 255))
+        userInput = pygame.key.get_pressed()
+
+        player.draw(SCREEN)
+        player.update(userInput)
+
+        if len(obstacles) == 0:
+            if random.randint(0, 2) == 0:
+                obstacles.append(SmallCactus(SMALL_CACTUS))
+            elif random.randint(0, 2) == 1:
+                obstacles.append(LargeCactus(LARGE_CACTUS))
+            elif random.randint(0, 2) == 2:
+                obstacles.append(Bird(BIRD))
+
+        for obstacle in obstacles:
+            obstacle.draw(SCREEN)
+            obstacle.update()
+            if player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(2000)
+                death_count += 1
+                menu(death_count)
+
+        background()
+
+        cloud.draw(SCREEN)
+        cloud.update()
+
+        score()
+
+        clock.tick(30)
+        pygame.display.update()
 
                
 
